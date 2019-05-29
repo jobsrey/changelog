61db825ac4abf6c0c36736967545734311e8f56e
kireniusdena <kireniusdena@idemas.id>
2019-05-29 11:09:55 +0700

    update update readmes

---
+++ ChangeLog.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace jobsrey\changelog;
+
+use yii;
+use yii\base\Component;
+use app\models\ChangeLog as ModelChangeLog;
+
+class ChangeLog extends Component
+{
+
+	//di gunakan di dalam fungsi afterSave ActiveRecord
+	public function saveLogUpdateByOne($attribute,$newData,$oldValue,$status = 2){
+		$attributesLabel						= $newData->attributeLabels();
+		$changeLogModel 						= new ModelChangeLog; //model change log
+		$changeLogModel->status 				= $status;
+		$changeLogModel->table_name 			= $newData->tableName();
+		$changeLogModel->model_name             = \yii\helpers\StringHelper::basename(get_class($newData));
+		$changeLogModel->alias_column_name 		= $attributesLabel[$attribute];
+		
+		//mencari field primary key
+		foreach($newData->primaryKey() as $key => $value){
+			$changeLogModel->id_record 				= $newData->{$value};
+		}
+
+		$changeLogModel->column_name 			= $attribute;
+		$changeLogModel->newvalue 				= $newData->{$attribute};
+		$changeLogModel->oldvalue 				= $oldValue[$attribute];
+		$changeLogModel->user_id 				= Yii::$app->user->identity->id;
+		$changeLogModel->parent_id              = Yii::$app->user->identity->parent_id;
+		$changeLogModel->save(false);
+	}
+
+
+	//digunakan untuk informasi saja bukan keterangan update file tapi hanya pesan saja
+	public function saveLogMessage($model,$message,$status = 3){
+		$attributesLabel						= $model->attributeLabels();
+		$changeLogModel 						= new ModelChangeLog; //model change log
+		$changeLogModel->status 				= $status;
+		$changeLogModel->table_name 			= $model->tableName();
+		$changeLogModel->model_name             = \yii\helpers\StringHelper::basename(get_class($model));
+		
+		//mencari field primary key
+		foreach($model->primaryKey() as $key => $value){
+			$changeLogModel->id_record 			= $model->{$value};
+		}
+
+		$changeLogModel->column_name 			= null;
+		$changeLogModel->newvalue 				= $message;
+		$changeLogModel->user_id 				= Yii::$app->user->identity->id;
+		$changeLogModel->parent_id              = Yii::$app->user->identity->parent_id;
+		$changeLogModel->save(false);
+	}
+}
---
+++ migrations/m190529_033435_create_table_change_log.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+class m190529_033435_create_table_change_log extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%change_log}}', [
+            'id' => $this->primaryKey(),
+            'table_name' => $this->string(),
+            'model_name' => $this->string(),
+            'newvalue' => $this->string(),
+            'oldvalue' => $this->string(),
+            'messages' => $this->string(),
+            'parent_id' => $this->integer(),
+            'user_id' => $this->integer(),
+            'is_active' => $this->smallInteger()->defaultValue('0'),
+            'created' => $this->dateTime(),
+            'updated' => $this->dateTime(),
+            'createby' => $this->integer(),
+            'updateby' => $this->integer(),
+            'status' => $this->integer()->notNull()->defaultValue('0'),
+            'alias_column_name' => $this->string(),
+            'id_record' => $this->integer()->notNull(),
+            'column_name' => $this->string(),
+        ], $tableOptions);
+
+        $this->createIndex('parent_id', '{{%change_log}}', 'parent_id');
+        $this->createIndex('user_id', '{{%change_log}}', 'user_id');
+        $this->createIndex('createby', '{{%change_log}}', 'createby');
+        $this->createIndex('updateby', '{{%change_log}}', 'updateby');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%change_log}}');
+    }
+}
